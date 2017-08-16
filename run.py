c67649239e41975e6c5998de33aa284b2c94bd24
COMOS <COMOS@CNSHI6P176.net.plm.eds.com>
2017-08-16 11:13:22 +0800

    birth

---
+++ run.py
@@ -0,0 +1,105 @@
+# -*- coding:utf-8 -*-
+from flask import Flask, render_template,redirect,jsonify,request
+# from dbORM import db,User, Post
+# import thumb
+from moduleGlobal import app,cache
+# from moduleGlobal import app, qiniu_store, QINIU_DOMAIN, TAG, UPLOAD_URL
+# import moduleAdmin as admin
+import flask_login
+import requests
+
+
+app.secret_key = '123456'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User( flask_login.UserMixin ):
+    def __init__(self,id):
+        self.id=id
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/light')
+@flask_login.login_required
+def light():
+    return render_template("light.html")
+
+###################APIS#######################
+@app.route('/api/light', methods=['POST','GET'])
+@flask_login.login_required
+def lightApi():
+    if request.method == 'POST':
+        lightStatus = request.form['lightStatus']
+        globalStatus = request.form['globalStatus']
+        rawData=[float(globalStatus),float(lightStatus)]
+        if lightStatus=="1":
+            cache.set("LIGHT_1_U",rawData)
+            print cache.get("LIGHT_1_U")
+
+        else:
+            cache.set("LIGHT_1_U", rawData)
+            print cache.get("LIGHT_1_U")
+
+
+
+    rawData = cache.get("LIGHT_1_D")
+
+    return jsonify({'status':'on','data':rawData})
+@app.route('/api/login', methods=['POST','GET'])
+def loginApi():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+        r = requests.get("http://128.1.67.182/api/verify?username=" + username + "&psswd=" + password)
+        result = r.json()
+        if result['status'] == "ok":
+            # login
+            user = User(int(result["content"]["uid"]))
+            flask_login.login_user(user)
+            # return redirect(url_for('protected'))
+            return jsonify({"status": "ok", "msg": "登陆成功"})
+        # TODO:other reasons
+        else:
+            return jsonify({"status": "failed", "msg": "登陆失败"})
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+
+
+# @app.route('/api/test', methods=['POST','GET'])
+# def testApi():
+#     if request.method=='POST':
+#         uid = request.form['uid']
+#         name = request.form['name']
+#         cache.delete('testData')
+#         cache.set('testData',{"uid":uid,'name':name})
+
+#         return jsonify({'status':'ok','content':{"uid":uid,'name':name}})
+#     if   request.method == 'GET':
+#         rv = cache.get('testData')
+#         print 'get '
+#         if rv is None:
+#             return jsonify({'status': 'error'})
+#         uid = rv['uid']
+#         name = rv['name']
+#         return jsonify({'status':'ok','content':{"uid":uid,'name':name}})
+
+
+application = app
+if __name__ == '__main__':
+    app.run(port=8085)
