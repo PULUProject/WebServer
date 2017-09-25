91204f7a520c8802f4529c00faadf6e758bca087
superdun <475098936@qq.com>
2017-09-26 00:11:24 +0800

    'fix'

--- gun.py
+++ gun.py
@@ -8,14 +8,14 @@ bind = '127.0.0.1:8085'
 #reload = True
 preload_app = True
 # debug when development and error when production
-loglevel = 'error'
-logfile = 'log/debug.log'
-accesslog = 'log/access.log'
+# loglevel = 'error'
+# logfile = 'log/debug.log'
+# accesslog = 'log/access.log'
 access_log_format = '%(h)s %(t)s %(U)s %(q)s'
-errorlog = 'log/error.log'
+# errorlog = 'log/error.log'
 # process name
 proc_name = 'webserver'
-pidfile = 'log/gunicorn.pid'
+# pidfile = 'log/gunicorn.pid'
 # set process daemon, not use in default
 #daemon = True
 # number of processes
@@ -23,3 +23,5 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # number of threads of per process
 threads = multiprocessing.cpu_count() * 2
 worker_class = 'gevent'
+
+
